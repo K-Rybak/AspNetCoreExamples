d9f42f13282556eecb22312f887d1289525872c0
Konstantin Rybak <62746770+K-Rybak@users.noreply.github.com>
2024-11-17 17:22:08 +0500

    added get methods

--- MinimalsAPI/CouponApi/Program.cs
+++ MinimalsAPI/CouponApi/Program.cs
@@ -1,3 +1,6 @@
+using CouponApi.Data;
+using CouponApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -8,6 +11,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/api/coupons", () => CouponStore.coupons);
+
+app.MapGet("/api/coupons/{id:int}", (int id) => 
+    CouponStore.coupons.FirstOrDefault(x => x.Id == id));
+
+app.MapGet("/api/coupons/{isActive:bool}", GetIsActiveCoupons);
 
 app.Run();
+
+List<Coupon> GetIsActiveCoupons(bool isActive)
+{
+    return CouponStore.coupons.Where(x => x.IsActive == isActive).ToList();
+}
